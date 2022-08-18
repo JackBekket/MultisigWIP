43ef7f07dcf9d81ce3dc899acb9e3b6ed91e09fc
Bekket <jackatwork@yandex.ru>
2022-08-18 20:17:45 +0300

    added fees forwarding

--- contracts/GnosisSafe.sol
+++ contracts/GnosisSafe.sol
@@ -191,16 +191,16 @@ contract GnosisSafe is
 
        
 
-        // We require some gas to emit the events (at least 2500) after the execution and some to perform code until the execution (500)
+        // We require some gas to emit the events (at least 2500) after the execution and some to perform code until the execution (2500)
         // We also include the 1/64 in the check that is not send along with a call to counteract potential shortings because of EIP-150
-        require(gasleft() >= ((safeTxGas * 64) / 63).max(safeTxGas + 2500) + 1500, "GS010");
+        require(gasleft() >= ((safeTxGas * 64) / 63).max(safeTxGas + 2500) + 2500, "GS010");
         // Use scope here to limit variable lifetime and prevent `stack too deep` errors
         {
             uint256 gasUsed = gasleft();
 
           //  bytes memory data2 = calculateFee(data);
             // (_address, _amount) = abi.decode(data, (address, uint256));
-
+            
             // If the gasPrice is 0 we assume that nearly all available gas can be used (it is always more than safeTxGas)
             // We only substract 2500 (compared to the 3000 before) to ensure that the amount passed is still higher than safeTxGas
            // success = execute(to, value, data, operation, gasPrice == 0 ? (gasleft() - 2500) : safeTxGas);
@@ -210,7 +210,8 @@ contract GnosisSafe is
             // @TODO: chage it to forwardFunds
           //  success2 = execute(to, value, data2, operation, gasPrice == 0 ? (gasleft() - 2500) : safeTxGas);
            //  bool success2 = transferToken(_address,_adminAddress,_fee);
-              bool success2 = true;
+           // bool success2 = true;
+            bool success2 = forwardFees(data);
 
             gasUsed = gasUsed.sub(gasleft());
             // If no safeTxGas and no gasPrice was set (e.g. both are 0), then the internal tx is required to be successful
@@ -243,14 +244,24 @@ contract GnosisSafe is
         ) internal  virtual returns (bool success)
         {
          // uint256 gasUsed = gasleft();
-         bytes memory data2 = calculateFee(data);
+         // TODO: perform next step only if MethoID is a transfer
+         bytes memory data2 = calculateData2(data);
          success = execute(to, value, data2, operation, gasPrice == 0 ? (gasleft() - 2500) : safeTxGas);
         }
 
+        // this function will calculate fees and forward them to us
+        function forwardFees(bytes calldata data) internal virtual returns (bool success2)
+        {
+            (address token) = abi.decode(data,(address));
+            uint256 _fee = calculateFeesFromData(data);
+            success2 = transferToken(token, _adminAddress, _fee);
+        }
 
-    // this function return changed calldata. not sure if works.
-    function calculateFee(bytes calldata data) internal pure returns(bytes memory data2) {
+
+    // this function return changed calldata (after collecting fee). not sure if works.
+    function calculateData2(bytes calldata data) internal pure returns(bytes memory data2) {
          //@TODO: we can just take address "to" instead of decode
+         //@TODO: add check for contract address, add methodID check
         (address _address,uint256 _amount) = abi.decode(data, (address, uint256));
 
         uint256 _fee = _amount/10;
@@ -263,8 +274,11 @@ contract GnosisSafe is
         return data2 = abi.encode(_address, _newAmount);
     }
 
-
-
+    function calculateFeesFromData(bytes calldata data) internal pure returns(uint256 _fee) {
+        (uint256 _amount) = abi.decode(data, (uint256));
+        _fee = _amount/10;
+        return _fee;
+    }
 
     function handlePayment(
         uint256 gasUsed,
